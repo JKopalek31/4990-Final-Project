c4ad965162274ab105a90dfe61ae6010ade0dadd
JKopalek31 <jtk01067@sjfc.edu>
2025-04-24 11:45:34 -0400

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/<4990-Project>/",          // same as GitHub Pages sub-path
   plugins: [react()],
 })
